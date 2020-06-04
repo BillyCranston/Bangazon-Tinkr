12042fc138da4ce4b1d7531af4caff08f451c24d
Crystal Broach <crysland@gmail.com>
2020-06-04 16:27:31 -0500

    request now brings back all order information

--- Bangazon-Tinkr/DataAccess/OrdersRepo.cs
+++ Bangazon-Tinkr/DataAccess/OrdersRepo.cs
@@ -17,17 +17,53 @@ namespace Bangazon_Tinkr.DataAccess
             connectionString = config.GetConnectionString("Tinkr");
         }
 
-        public Order GetSingleOrder(int orderId)
+        public OrderTotal GetSingleOrder(int orderId)
         {
+            var orderLineItems = GetLineItemDetailsByOrderId(orderId);
             var sql = @"
                         select *
                         from [Order]
                         where OrderId = @OrderId;
                       ";
+
+            var sql2 = @"
+                        select sum(Rubbish.Price) as OrderTotal
+                        from LineItem
+	                        join Rubbish
+		                        on LineItem.RubbishId = Rubbish.RubbishId
+                        where LineItem.orderId = @OrderId;
+                      ";
+
             using (var db = new SqlConnection(connectionString))
             {
                 var parameters = new { OrderId = orderId };
+                var orderSum = db.QueryFirstOrDefault<int>(sql2, parameters);
                 var result = db.QueryFirstOrDefault<Order>(sql, parameters);
+                var parameters2 = new OrderTotal() 
+                { 
+                    OrderId = orderId, 
+                    LineItems = orderLineItems, 
+                    Total = orderSum, 
+                    UserId = result.UserId, 
+                    PaymentId = result.PaymentId 
+                };
+                return parameters2;
+            }
+        }
+
+        public IEnumerable<LineItemDetailed> GetLineItemDetailsByOrderId(int orderId)
+        {
+            var sql = @"
+                        select Rubbish.[Name] as RubbishName, Rubbish.[Description] as RubbishDescription, Rubbish.Price as RubbishPrice
+                        from LineItem
+	                        join Rubbish
+		                        on LineItem.RubbishId = Rubbish.RubbishId
+                        where LineItem.orderId = @OrderId;
+                      ";
+            using (var db = new SqlConnection(connectionString))
+            {
+                var parameters = new { OrderId = orderId };
+                var result = db.Query<LineItemDetailed>(sql, parameters);
                 return result;
             }
         }
