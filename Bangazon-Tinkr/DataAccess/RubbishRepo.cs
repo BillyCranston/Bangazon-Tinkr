d2c7252ff8a77e2e941e233baca15a40b93bdc50
Connor Sullivan <cksullvn@gmail.com>
2020-06-06 10:25:46 -0500

    get single rubbish works

--- Bangazon-Tinkr/DataAccess/RubbishRepo.cs
+++ Bangazon-Tinkr/DataAccess/RubbishRepo.cs
@@ -3,6 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bangazon_Tinkr.Models;
+using Dapper;
+using System.Data.SqlClient;
 
 namespace Bangazon_Tinkr.DataAccess
 {
@@ -12,6 +15,19 @@ namespace Bangazon_Tinkr.DataAccess
         public RubbishRepo(IConfiguration config)
         {
             connectionString = config.GetConnectionString("Tinkr");
+        }
+
+        public Rubbish getSingleRubbish(int rubbishId)
+        {
+            var sql = @"SELECT * FROM Rubbish
+                        WHERE RubbishId = @RubbishId;";
+
+            using (var db = new SqlConnection(connectionString))
+            {
+                var parameters = new { RubbishId = rubbishId };
+                var rubbish = db.QueryFirstOrDefault<Rubbish>(sql, parameters);
+                return rubbish;
+            }
         }
     }
 }
