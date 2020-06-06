5f3c47b7b3bb050cc0233235be0ea85767e30e39
alesha.reed <aleshak83@gmail.com>
2020-06-06 10:30:50 -0500

    Get all rubbish

--- Bangazon-Tinkr/Controllers/RubbishController.cs
+++ Bangazon-Tinkr/Controllers/RubbishController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bangazon_Tinkr.DataAccess;
+using Bangazon_Tinkr.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Bangazon_Tinkr.Controllers
 {
@@ -10,6 +13,20 @@ namespace Bangazon_Tinkr.Controllers
     [ApiController]
     public class RubbishController : ControllerBase
     {
-        
+        RubbishRepo _rubbishRepository;
+        public RubbishController(RubbishRepo repository)
+        {
+            _rubbishRepository = repository;
+        }
+
+        // api/Rubbish/
+        [HttpGet]
+        public IActionResult GetAllRubbish()
+        {
+            var allRubbish = _rubbishRepository.GetRubbish();
+            if (allRubbish == null) return NotFound(" There is not any Rubbish in the inventory");
+
+            return Ok(allRubbish);
+        }
     }
 }
--- Bangazon-Tinkr/DataAccess/RubbishRepo.cs
+++ Bangazon-Tinkr/DataAccess/RubbishRepo.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Bangazon_Tinkr.Models;
+using Dapper;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +15,14 @@ namespace Bangazon_Tinkr.DataAccess
         public RubbishRepo(IConfiguration config)
         {
             connectionString = config.GetConnectionString("Tinkr");
+        }
+
+        public List<Rubbish> GetRubbish()
+        {
+            using (var db = new SqlConnection(connectionString))
+            {
+                return db.Query<Rubbish>("select * from rubbish").ToList();
+            }
         }
     }
 }
