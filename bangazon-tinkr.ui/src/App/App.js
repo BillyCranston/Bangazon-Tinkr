00baf44eb078f21c0d8600914958990827ffe821
Crystal Broach <crysland@gmail.com>
2020-06-13 16:06:34 -0500

    react setup, structure and routes complete

--- bangazon-tinkr.ui/src/App/App.js
+++ bangazon-tinkr.ui/src/App/App.js
@@ -1,12 +1,62 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+
+import MainNavbar from '../components/shared/MainNavbar/MainNavbar';
+
+import Home from '../components/pages/Home/Home';
+import Products from '../components/pages/Products/Products';
+import Profile from '../components/pages/Profile/Profile';
+import ShoppingCart from '../components/pages/ShoppingCart/ShoppingCart';
+import SingleProduct from '../components/pages/SingleProduct/SingleProduct';
+
 import './App.scss';
 
-function App() {
-  return (
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+// const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+//   const routeChecker = (props) => (authed === true ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/auth', state: { from: props.location } }} />);
+//   return <Route {...rest} render={(props) => routeChecker(props)} />;
+// };
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  // componentDidMount() {
+  //   // add authentication here when ready
+  //   this.setState({ authed: true });
+  // }
+
+  // componentWillUnmount() {
+  //   this.removeListener();
+  // }
+
+  render() {
+    const { authed } = this.state;
+    return (
     <div className="App">
-      <button className="btn btn-info">Bootstrap Button</button>
+      <Router>
+        <MainNavbar authed={authed} />
+        <Switch>
+          <PublicRoute path="/" exact component={Home} authed={authed} />
+          <PublicRoute path="/profile" exact component={Profile} authed={authed} />
+          <PublicRoute path="/shoppingCart" exact component={ShoppingCart} authed={authed} />
+          <PublicRoute path="/products" exact component={Products} authed={authed} />
+          <PublicRoute path="/product/:productId" exact component={SingleProduct} authed={authed} />
+        </Switch>
+      </Router>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
