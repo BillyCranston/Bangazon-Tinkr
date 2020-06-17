ff02e59d6ce42e73aaafc637975dbecde0ad7f0c
jthielman <john.thielman@gmail.com>
2020-06-16 20:21:54 -0500

    the page changed a little!

--- bangazon-tinkr.ui/src/components/pages/SingleProduct/SingleProduct.js
+++ bangazon-tinkr.ui/src/components/pages/SingleProduct/SingleProduct.js
@@ -6,7 +6,12 @@ class SingleProduct extends React.Component {
   render() {
     return (
       <div className="SingleProduct">
-        <h1>Single Product Page</h1>
+        <h1>Product Name</h1>
+        {/* title, description, quantity available, price per unit, and a button labeled Add to Cart */}
+        <p>Description</p>
+        <p>Quantity available</p>
+        <p>Price:</p>
+        <button>Add to Cart</button>
       </div>
     );
   }
