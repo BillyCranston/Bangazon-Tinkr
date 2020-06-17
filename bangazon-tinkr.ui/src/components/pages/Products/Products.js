1eb49584434d700cccbd354dbaac66a636aa7536
Crystal Broach <crysland@gmail.com>
2020-06-16 21:34:47 -0500

    product card data printing cards from db

--- bangazon-tinkr.ui/src/components/pages/Products/Products.js
+++ bangazon-tinkr.ui/src/components/pages/Products/Products.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import ProductCard from '../../shared/ProductCard/ProductCard';
 
+import productData from '../../../helpers/data/productData';
+
 import './Products.scss';
 
 class Products extends React.Component {
+  state = {
+    products: [],
+  }
+
+  getProducts = () => {
+    productData.getProducts()
+      .then((products) => {
+        this.setState({ products });
+      })
+      .catch((err) => console.error('error from get products', err));
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  renderProductView = () => {
+    const { products } = this.state;
+    if (products.length !== 0) {
+      return (
+        products.map((product) => <ProductCard key={product.id} product={product}/>)
+      );
+    }
+    return (
+      <h3>There are currently no items available.</h3>
+    );
+  }
+
   render() {
     return (
       <div className="Products">
         <h1>All Products Component</h1>
-        <ProductCard />
-        <ProductCard />
+        {this.renderProductView()}
       </div>
     );
   }
--- bangazon-tinkr.ui/src/components/shared/ProductCard/ProductCard.js
+++ bangazon-tinkr.ui/src/components/shared/ProductCard/ProductCard.js
@@ -3,16 +3,24 @@ import React from 'react';
 import './ProductCard.scss';
 
 class ProductCard extends React.Component {
+  // static propTypes = {
+  //   product: productShape.productShape,
+  // }
+
+  // componentDidMount() {
+  //   const { product } = this.props;
+  // }
+
   render() {
+    const { product } = this.props;
     return (
       <div className="ProductCard">
-        <h1>Product Card</h1>
         <div className="card col-3">
-        <img src="..." class="card-img-top cardImage" alt="..." />
+        <img src="..." className="card-img-top cardImage" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">Product Name</h5>
-            <h6 className="card-subtitle mb-2 text-muted">Product Price</h6>
-            <p className="card-text">Brief Description of the item for purchase.</p>
+          <h5 className="card-title">{product.name}</h5>
+    <h6 className="card-subtitle mb-2 text-muted">${product.price}</h6>
+            <p className="card-text">{product.description}</p>
             <a href="#" className="card-link btn btn-success">Add To Cart</a>
             <a href="#" className="card-link btn btn-primary">More Info...</a>
           </div>
