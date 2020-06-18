db7f8f704b64b547ebdd5489635b8dcd24abc69c
alesha.reed <aleshak83@gmail.com>
2020-06-17 20:07:05 -0500

    added dropdown

--- bangazon-tinkr.ui/src/components/pages/Home/Home.js
+++ bangazon-tinkr.ui/src/components/pages/Home/Home.js
@@ -1,26 +1,26 @@
 import React from 'react';
-
+import Button from '../../shared/Button/Button';
 import './Home.scss';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="Home d-flex justify-content-center align-items-center">
-        <div class=" home-container container d-flex justify-content-center align-items-center">
-          <div class="row">
-            <div class=" home-content col d-flex justify-content-center align-items-center">
-            <div class="jumbotron jumbotron-fluid">
-              <div class="container">
-                <h1 class="display-4">Rubbish Refurbished</h1>
-                <p class="lead">Tinkr is the place for amazing antiques, rustic relics, fantastic finds, broken bits, and everything in between. Simply search by name or category to find your next fix. </p>
+        <div className=" home-container container d-flex justify-content-center align-items-center">
+          <div className="row">
+            <div className=" home-content col d-flex justify-content-center align-items-center">
+            <div className="jumbotron jumbotron-fluid">
+              <div className="container">
+                <h1 className="display-4">Rubbish Refurbished</h1>
+                <p className="lead">Tinkr is the place for amazing antiques, rustic relics, fantastic finds, broken bits, and everything in between. Simply search by name or category to find your next fix. </p>
               </div>
             </div>
             </div>
-            <div class=" home-content col d-flex justify-content-center align-items-center">
-              <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Find something to Tinkr with..." aria-label="tinkrSearch" aria-describedby="basic-addon2"/>
-                  <div class="input-group-append">
-                    <button class="btn btn-outline-secondary" type="button">Search</button>
+            <div className=" home-content col d-flex justify-content-center align-items-center">
+              <div className="input-group mb-3">
+                <input type="text" className="form-control" placeholder="Find something to Tinkr with..." aria-label="tinkrSearch" aria-describedby="basic-addon2"/>
+                  <div className="input-group-append">
+                    <Button />
                   </div>
               </div>
             </div>
