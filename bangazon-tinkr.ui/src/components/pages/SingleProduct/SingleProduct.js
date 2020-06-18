2298ae81fa0418d08a22387d55ef4c7903f11bb4
jthielman <john.thielman@gmail.com>
2020-06-17 21:14:04 -0500

    single product page looks okay mostly

--- bangazon-tinkr.ui/src/components/pages/SingleProduct/SingleProduct.js
+++ bangazon-tinkr.ui/src/components/pages/SingleProduct/SingleProduct.js
@@ -11,7 +11,7 @@ class SingleProduct extends React.Component {
 
   getRubbish = (rubbishId) => {
     productData.getRubbishById(rubbishId)
-      .then((response) => /* console.log(response))// */this.setState({ rubbish: response }))
+      .then((response) => this.setState({ rubbish: response }))
       .catch((err) => console.error('error in get rubbish', err));
   }
 
@@ -24,13 +24,19 @@ class SingleProduct extends React.Component {
     // const { rubbishId } = this.props.match.params;
     const { rubbish } = this.state;
     return (
-      <div className="SingleProduct">
-        <h2>Product Name {rubbish.name}</h2>
-        {/* title, description, quantity available, price per unit, and a button labeled Add to Cart */}
-        <p>Description</p>
-        <p>Quantity available</p>
-        <p>Price:</p>
-        <button>Add to Cart</button>
+      <div className="SingleProduct container">
+        <div className="row d-flex flex-wrap">
+          <div className="col">
+            <img src="https://image.shutterstock.com/image-vector/ui-image-placeholder-wireframes-apps-260nw-1037719204.jpg"/>
+          </div>
+          <div className="col">
+            <h2>{rubbish.name}</h2>
+            <p>{rubbish.description}</p>
+            <p>{rubbish.isAvailable ? "Still" : "No longer"} available</p>
+            <p>Price: ${rubbish.price}.00</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
       </div>
     );
   }
